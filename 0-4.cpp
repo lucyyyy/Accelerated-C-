b3a7b4fc04f71ef1690d50200100177734a9c58b
lucyyyy <644156399@qq.com>
2017-10-12 11:00:52 +0800

    Add files via upload

---
+++ 0-4.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "#include <iostream>" << endl;
+    cout << "\n" << endl;
+    cout << "using namespace std;"<< endl;
+    cout << "\n" << endl;
+    cout << "int main()"<< endl;
+    cout << "{"<< endl;
+    cout << "   cout<<\"Hello, world!\"<<endl;"<< endl;
+    cout << "   return 0;"<< endl;
+    cout << "}"<< endl;
+
+    return 0;
+}
