29956163578ce6177dc127c5b3bba60be78e4a0f
lucyyyy <644156399@qq.com>
2017-10-12 11:13:57 +0800

    Add files via upload

---
+++ 0-7&0-8.cpp
@@ -0,0 +1,24 @@
+// These two questions talk about the usage of comments
+//0-7
+#include <iostream>
+
+int main() {
+    /*This is a command line....
+    because it uses /* and */ as its and ending */
+    std::cout<<"Hello, world!"<< std::endl;
+    return 0;}
+// This example is not valid, since the comment part ends with the first */
+
+//0-8
+
+#include <iostream>
+
+int main() {
+    //This is a command line
+    //by using //at the beginning ,*/
+    //or*/ to limit
+    std::cout<<"Hello, world!"<< std::endl;
+    return 0;}
+
+//This one in valid, since if beginning with the //, all things following is comment
+
